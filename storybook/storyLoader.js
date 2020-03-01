d00c11c90168e31643ab40e8883ef9d427bf5a9c
hungtd <hungtdqn96@gmail.com>
2020-03-01 12:10:41 +0700

    initial

---
+++ storybook/storyLoader.js
@@ -0,0 +1,25 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+	require('../src/components/AppFooter/stories/index.stories');
+	require('../src/components/AppHeader/stories/index.stories');
+	require('../src/forms/LoginForm/stories/index.stories');
+	require('../src/forms/formFields/SelectInput/stories/index.stories');
+	require('../src/forms/formFields/TextInput/stories/index.stories');
+}
+
+const stories = [
+	'../src/components/AppFooter/stories/index.stories',
+	'../src/components/AppHeader/stories/index.stories',
+	'../src/forms/LoginForm/stories/index.stories',
+	'../src/forms/formFields/SelectInput/stories/index.stories',
+	'../src/forms/formFields/TextInput/stories/index.stories'
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
